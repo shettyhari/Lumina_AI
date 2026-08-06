2c508deef7dfd392c18e634f5ea7fee8dc4885b1
shettyhari <theebg42@gmail.com>
2026-08-06 17:03:57 +0530

    fix: use dynamic import in api/index.ts to fix ERR_REQUIRE_ESM on Vercel

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,6 @@
-// @ts-ignore
-import app from "../artifacts/api-server/dist/app.mjs";
-
-export default app;
+export default async function handler(req: any, res: any) {
+  // @ts-ignore
+  const appModule = await import("../artifacts/api-server/dist/app.mjs");
+  const app = appModule.default || appModule;
+  return app(req, res);
+}
